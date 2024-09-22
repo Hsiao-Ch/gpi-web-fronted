dcd306b283fa5e92ed2e1a65b915dc2f3ae5e39d
hsiao <10856003@ntub.edu.tw>
2024-09-22 15:37:32 +0800

    新增 - footer

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import logo from "@/public/GPi logo.jpg";
+import { FaArrowRight, FaFacebook } from "react-icons/fa";
+import { FooterLinkData } from "@/lib/data/footerData";
+import styles from "@/styles/components/layout/footer.module.scss";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className={styles.container}>
+      <div className={styles.footerWrap}>
+        <div>
+          <h2 className={styles.logo}>GPi</h2>
+          {/* <Image className={styles.logo} src={logo} alt="GPI-LOGO" /> */}
+          <p style={{ fontWeight: "bold" }}>
+            GREEN PARTNERS INDUSTRY CO., LTD.
+          </p>
+          <p>新北市新莊區新北大道4段217號3樓</p>
+          <p>電話: 02-85213289</p>
+          <p>傳真: 02-81926810</p>
+          <p></p>
+        </div>
+        <div>
+          <h2>相關連結</h2>
+          <div className={styles.links}>
+            {FooterLinkData.map((item, index) => (
+              <Link href={item.link}>
+                <FaArrowRight />
+                {item.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2>關於我們</h2>
+          <p className={styles.aboutContent}>
+            我們主要從事空調設備，代理進口空調設備冷卻水塔及水處理設備；包含冷卻水
+            塔、雜質分離器、微氣泡分離器、水處理設備等。本公司產品於國內外市場擁有廣大使
+            用群，國內代表性企業皆為長期使用客戶。
+          </p>
+          <Link href={""}>閱讀更多</Link>
+        </div>
+      </div>
+      <a href={'https://www.facebook.com/Marleycoolingtower/'}><FaFacebook  className={styles.fbIcon} /></a>
+    </footer>
+  );
+}
---
+++ lib/data/footerData.ts
@@ -0,0 +1,12 @@
+export const FooterLinkData = [
+    {'name': '關於我們', 'link': ''},
+    {'name': '獎項與證書', 'link': ''},
+    {'name': '加入我們', 'link': ''},
+    {'name': '環境、健康與安全政策', 'link': ''},
+    {'name': '最新消息', 'link': ''},
+    {'name': '我們的團隊', 'link': ''},
+    {'name': '組織架構', 'link': ''},
+    {'name': '活動專區', 'link': ''},
+    {'name': '新聞稿專區', 'link': ''},
+    {'name': '策略聯盟', 'link': ''}
+]
